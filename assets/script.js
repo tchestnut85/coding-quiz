ef8d1df7458f28c7ad40b1e928a8d5db3f320b07
Tom Chestnut <tchestnut85@gmail.com>
2020-08-28 22:16:51 -0400

    added timer function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
 var timerClock = document.getElementById("timer");
 var quizSection = document.getElementById("quiz");
+var startButton = document.getElementById("button");
 
 function timer() {
-    var remainingTime = 30
-}
+    var remainingTime = 30;
+
+    var interval = setInterval(function() {
+        if (remainingTime >= 0) {
+            timerClock.textContent = remainingTime;
+            remainingTime--;
+        } else {
+            // figure out later
+        }
+    }, 1000);
+};
+
+startButton.onclick = timer;
