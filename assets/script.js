88ed6366e4b495d75f0236533e0e1f2df7c60bc7
Tom Chestnut <tchestnut85@gmail.com>
2020-08-29 21:57:42 -0400

    updated ability to advance through questions

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,11 @@ var quizSection = document.getElementById("quiz");
 var startButton = document.getElementById("button");
 var quizWrapper = document.getElementById("quiz-wrapper");
 var answerList = document.getElementById("answer-list");
+var intro = document.getElementById("intro");
 
-var currentQuestion = 0;
 var scoreCounter = 60;
+var remainingTime = scoreCounter;
+var currentQuestion = 0;
 
 
 var questions = [ 
@@ -15,24 +17,24 @@ var questions = [
         correct: "strings"
     },
     {
-        question: "Quetion 2: Which of the following should be link CSS styling to HTML elements?", 
+        question: "Question 2: Which of the following should link CSS styling to HTML elements?", 
         choices: ["classes", "id", "flexbox", "grid"],
         correct: "classes"
     },
     {
-        question: "Question 3: Write out question in here...", 
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 3: What file must be in a directory for a website to function?", 
+        choices: ["style.js", "README.md", "index.html", "style.css"],
+        correct: "index.html"
     },
     {
-        question: "Question 4: Write out question in here...", 
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 4: Arrays in JavaScript can be used to store:", 
+        choices: ["strings", "integers", "other arrays", "all of the above"],
+        correct: "all of the above"
     },
     {
-        question: "Question 5: Write out question in here...", 
-        choices: ["answer1", "answer2", "answer3", "answer4"],
-        correct: "answer1"
+        question: "Question 5: The condition in an if/else statement must be enclosed in:", 
+        choices: ["quotes", "curley braces", "parenthesis", "commas"],
+        correct: "parenthesis"
     }
 ];
 
@@ -41,20 +43,19 @@ var questions = [
 // TIMER FUNCTION
 // Clicking the Start button begins timer countdown and displays 1 of 5 questions
 function timer() {
-    var remainingTime = scoreCounter;
-
     var interval = setInterval(function() {
         if (remainingTime > 0) {
             timerClock.textContent = remainingTime;
             remainingTime--;
         } else {
             clearInterval(interval);
-            // show high score page
+            // show high score page - how to link another html page through a function?
         }
     }, 1000);
 }
 
 function getQuestion() {
+    intro.remove();
     var question = questions[currentQuestion];
     document.getElementById("question").textContent = question.question;
     answerList.innerHTML = "";
@@ -68,20 +69,21 @@ function getQuestion() {
 
 function questionClick() {
     var question = questions[currentQuestion];
-    console.log(this);
+    // console.log(this);
     if (question.correct === this.textContent) {
         var correctAlert = document.createElement("p"); 
         correctAlert.textContent = "YOU ANSWERED CORRECT! YOU'RE SO SMART!!";
         quizWrapper.appendChild(correctAlert);
-    } else if {
-        scoreCounter -= 20;
-    }
-    // 0 1 2 3 4 , when current question = 4
-    currentQuestion++ 
-    if (currentQuestion === questions.length) {
+        currentQuestion++;
+    } else if (question.correct = !this.textContent) {
+        remainingTime = remainingTime - 20;
+        currentQuestion++;
         // show high score page
+        // remove(correctAlert);
+    } else {
+        currentQuestion++;
+        // currentQuestion === questions.length;
     }
-    // remove(correctAlert);
     getQuestion();
 }
 
@@ -99,5 +101,4 @@ startButton.addEventListener("click", timer);
 // If a question is answered wrong, time is subtracted from the timer
 
 // When timer runs out, you are brought to the high score pag
-// and can save your initials in localStorae
-
+// and can save your initials in localStorage
